ebe17d5a112a1eb8578328b100f95f4c385eb5ae
Cyber-Kaeh <153845457+Cyber-Kaeh@users.noreply.github.com>
2024-12-07 11:28:06 -0500

    Updated projects page and included dynamic modal. Going to try to install marked and parse MD files for description next.

---
+++ src/components/ProjectModal.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    v-if="show"
+    class="modal fade show"
+    tabindex="-1"
+    style="display: block"
+    @click.self="close"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ title }}</h5>
+          <button type="button" class="btn-close" @click="close"></button>
+        </div>
+        <div class="modal-body">
+          <p>{{ description }}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" @click="close">
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      show: {
+        type: Boolean,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+    },
+    methods: {
+      close() {
+        this.$emit("close")
+      },
+    },
+  }
+</script>
+
+<style>
+  .modal {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,65 +4,93 @@
 
     <div class="container-sm">
       <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
+        <div class="col" v-for="project in projects" :key="project.id">
           <div class="card text-start h-100">
             <img
-              src="../assets/home-screenshot1.png"
+              :src="project.image"
               class="card-img-top"
-              alt="..."
+              :alt="project.title"
             />
             <div class="card-body">
-              <h5 class="card-header">This website!</h5>
-              <p class="card-text">
-                My personal website built with Vue.js and Bootstrap 5.
-              </p>
-              <a href="#" class="btn btn-primary">Details</a>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card text-start h-100">
-            <img
-              src="../assets/nextcloud-screenshot.png"
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-header">Private Cloud</h5>
-              <p class="card-text">
-                Self-hosted NextCloud server running on a Raspberry Pi 4.
-              </p>
-              <a href="#" class="btn btn-primary">Details</a>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card text-start h-100">
-            <img
-              src="../assets/bitwarden-og.webp"
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-header">Password Manager</h5>
-              <p class="card-text">
-                Self-hosted Bitwarden instance host in AWS EC2.
-              </p>
-              <a href="#" class="btn btn-primary">Details</a>
+              <h5 class="card-header">{{ project.title }}</h5>
+              <p class="card-text">{{ project.shortDescription }}</p>
+              <button class="btn btn-primary" @click="openModal(project)">
+                Details
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <Modal
+      v-if="selectedProject"
+      :show="showModal"
+      :title="selectedProject.title"
+      :description="selectedProject.description"
+      @close="closeModal"
+    />
   </div>
 </template>
 
 <script>
-  export default {}
+  import ProjectModal from "@/components/ProjectModal.vue"
+
+  export default {
+    components: {
+      Modal: ProjectModal,
+    },
+    data() {
+      return {
+        showModal: false,
+        selectedProject: null,
+        projects: [
+          {
+            id: 1,
+            title: "This website!",
+            shortDescription:
+              "My personal website built with Vue.js and Bootstrap 5.",
+            description: "Detailed description of this website project...",
+            image: require("../assets/home_screenshot.png"),
+          },
+          {
+            id: 2,
+            title: "Nextcloud",
+            shortDescription: "A self-hosted cloud storage solution.",
+            description: "Detailed description of the Nextcloud project...",
+            image: require("../assets/nextcloud-screenshot.png"),
+          },
+          {
+            id: 3,
+            title: "Chuck Jokes",
+            shortDescription:
+              "A simple web app that displays endless Chuck Norris jokes!",
+            description:
+              "This is a fun and easy project I did to learn more about using Flask, APIs, and Docker. It was incredibly fun to bring it all together and deploy it locally using my Raspberry Pi. It is also super fun to show to visitors or for just a little midday pick-me-up with a Chuck Norris joke.",
+            image: require("../assets/chuckapp_screenshot.png"),
+          },
+          // Add more projects as needed
+        ],
+      }
+    },
+    methods: {
+      openModal(project) {
+        this.selectedProject = project
+        this.showModal = true
+      },
+      closeModal() {
+        this.selectedProject = null
+        this.showModal = false
+      },
+    },
+  }
 </script>
 
 <style scoped>
   .projects-container {
     padding: 20px;
+  }
+  .modal {
+    background-color: rgba(0, 0, 0, 0.5);
+    display: none;
   }
 </style>
