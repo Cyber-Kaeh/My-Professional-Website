4201fd6c86971918462d80a43fe97338633c789a
Cyber-Kaeh <153845457+Cyber-Kaeh@users.noreply.github.com>
2025-05-07 01:03:06 -0400

    Just cant quite get the scrollspy right. Decided to go with working links over active links.

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -43,6 +43,7 @@
         data-bs-spy="scroll"
         data-bs-target="#list-example"
         data-bs-smooth-scroll="true"
+        data-bs-offset="80"
         tabindex="0"
       >
         <section id="list-item-1" class="mb-4">
@@ -69,109 +70,230 @@
             <br />
             Working collaboratively with over 50 peers in the graduating IT
             class to design, develop, and implement a retail website
-            <ul>
-              <li>Analyze system specifications to architect a deliverable product</li>
-              <li>Implement development environments using GitHub Codespaces and VS Code – Dev Containers.</li>
-              <li>Document build procedures for environments on GitHub (View full codebase: <a href="https://github.com/Cyber-Kaeh/elevate-retail" target="_blank">https://github.com/Cyber-Kaeh/elevate-retail</a>).</li>
-              <li>Implement a containerized MSSQL instance in Docker for the test database while maintaining parity with the production database.</li>
-              <li>Troubleshoot errors or issues related to installation, environments, deployments, dependencies, database connections, or general IT-related problems for all teams.</li>
-              <li>Implement efficient, security-first coding practices using Python’s Flask framework, SQL-Alchemy, Werkzeug, and CSRF protection tokens.</li>
-              <li>Create an intuitive and responsive front-end using Bootstrap 5, Jinja templating, and AJAX methodology.</li>
-            </ul>
           </p>
+          <ul>
+            <li>
+              Analyze system specifications to architect a deliverable product
+            </li>
+            <li>
+              Implement development environments using GitHub Codespaces and VS
+              Code – Dev Containers.
+            </li>
+            <li>
+              Document build procedures for environments on GitHub (View full
+              codebase:
+              <a
+                href="https://github.com/Cyber-Kaeh/elevate-retail"
+                target="_blank"
+                >https://github.com/Cyber-Kaeh/elevate-retail</a
+              >).
+            </li>
+            <li>
+              Implement a containerized MSSQL instance in Docker for the test
+              database while maintaining parity with the production database.
+            </li>
+            <li>
+              Troubleshoot errors or issues related to installation,
+              environments, deployments, dependencies, database connections, or
+              general IT-related problems for all teams.
+            </li>
+            <li>
+              Implement efficient, security-first coding practices using
+              Python’s Flask framework, SQL-Alchemy, Werkzeug, and CSRF
+              protection tokens.
+            </li>
+            <li>
+              Create an intuitive and responsive front-end using Bootstrap 5,
+              Jinja templating, and AJAX methodology.
+            </li>
+          </ul>
           <h5>WEB APP DEVELOPER INTERN</h5>
           <p>
             Forsyth Technical Community College, Winston-Salem, NC
             <br />
             February 2024 - July 2024
             <br />
-            Working on a small team of student interns under the supervision of a senior software developer to create a statistical calculator for the school's math department.
-            <ul>
-              <li>Draw from prior knowledge of basic HTML/CSS to create a visual interface for testing statistical calculations.</li>
-              <li>Learn JavaScript to leverage the math.js library, allowing for fast processing of complex mathematical computations.</li>
-              <li>Work independently or with a small team to deliver functional code in a small window of time.</li>
-            </ul>
+            Working on a small team of student interns under the supervision of
+            a senior software developer to create a statistical calculator for
+            the school's math department.
           </p>
+          <ul>
+            <li>
+              Draw from prior knowledge of basic HTML/CSS to create a visual
+              interface for testing statistical calculations.
+            </li>
+            <li>
+              Learn JavaScript to leverage the math.js library, allowing for
+              fast processing of complex mathematical computations.
+            </li>
+            <li>
+              Work independently or with a small team to deliver functional code
+              in a small window of time.
+            </li>
+          </ul>
           <h5>RETAIL SUPERVISOR</h5>
           <p>
             Gabe's, Lexington, NC
             <br />
             September 2024 - March 2025
             <br />
-            Working in a high-volume retail outlet store to deliver support, value, and assistance to customers as well as team members.
-            <ul>
-              <li>Resolve escalated customer issues and provide equitable solutions.</li>
-              <li>Troubleshoot point of sale systems, hand-held devices, office computers, and printers. Escalating to the company helpdesk ticketing system if necessary.</li>
-              <li>Execute strategic plans to minimize shrinkage and enhance operational efficiency.</li>
-              <li>Successfully perform opening and closing functions, including cash office execution and alarm resolution during and after hours.</li>
-            </ul>
+            Working in a high-volume retail outlet store to deliver support,
+            value, and assistance to customers as well as team members.
           </p>
+          <ul>
+            <li>
+              Resolve escalated customer issues and provide equitable solutions.
+            </li>
+            <li>
+              Troubleshoot point of sale systems, hand-held devices, office
+              computers, and printers. Escalating to the company helpdesk
+              ticketing system if necessary.
+            </li>
+            <li>
+              Execute strategic plans to minimize shrinkage and enhance
+              operational efficiency.
+            </li>
+            <li>
+              Successfully perform opening and closing functions, including cash
+              office execution and alarm resolution during and after hours.
+            </li>
+          </ul>
         </section>
         <section id="list-item-3" class="mb-4">
           <h3>EDUCATION</h3>
-          <h5>Associate’s of Applied Science in IT, Computer Programming and Software Development</h5>
+          <h5>
+            Associate’s of Applied Science in IT, Computer Programming and
+            Software Development
+          </h5>
           <p>
             Forsyth Technical Community College, Winston-Salem, NC
             <br />
             Graduated: May 2025
             <br />
-            <ul>
-              <li>GPA 4.0</li>
-              <li>Distinguished member of the President's List</li>
-              <li>Relevant coursework: Python Programming, Networking Foundations, Database Management, Security Concepts, Ethics in High Technology, Systems Design and Analysis</li>
-            </ul>
+          </p>
+          <ul>
+            <li>GPA 4.0</li>
+            <li>Distinguished member of the President's List</li>
+            <li>
+              Relevant coursework: Python Programming, Networking Foundations,
+              Database Management, Security Concepts, Ethics in High Technology,
+              Systems Design and Analysis
+            </li>
+          </ul>
+          <h5>MICROSOFT CERTIFIED: AZURE DATAFUNDMENTALS (DP-900)</h5>
+          <p>
+            Credential ID: 1D65ED49149EED2F | Certification number:
+            20AECR-FA8560
+            <br />
+            February 2025
           </p>
         </section>
         <section id="list-item-4" class="mb-4">
-          <h4>Projects</h4>
+          <h3>PROJECTS</h3>
+          <h5>VIRTUALIZATION SERVER</h5>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi ea,
-            voluptatum impedit quidem, ratione quis suscipit natus quibusdam
-            placeat expedita laudantium illo eius totam eum iure. Quam, nam
-            itaque sunt molestias aspernatur, provident similique voluptatem
-            harum at fugit eos quis, incidunt doloremque esse inventore! Quo
-            itaque alias vitae et facere expedita harum deserunt eum non
-            laborum, est, optio perferendis nam ullam sunt voluptas. Mollitia
-            iste repellendus libero ut. Commodi nulla quia quae fuga nihil
-            debitis nemo voluptatem voluptatibus? A assumenda velit impedit, quo
-            quod magnam. Labore porro quas iure eaque cum veritatis magni quod
-            cupiditate! Ratione blanditiis asperiores sint corporis?
+            Implementation of Proxmox PVE 8 on a retired enterprise desktop
+            computer. Despite being limited to a 4th-generation i7 processor, it
+            easily hosts a combination of around eight separate virtual machines
+            and LXC containers. Currently managing self-hosted instances such as
+            Nextcloud private cloud, and Kasm Workspaces (ephemeral desktops or
+            appliances based on Docker).
           </p>
+          <h5>HOME NETWORK</h5>
+          <ul>
+            <li>
+              Router and switch management to provide network segmentation
+            </li>
+            <li>
+              Self-hosted Twingate connector to provide safe remote connections
+              without port-forwarding
+            </li>
+            <li>DNS Sinkhole provided by self-hosted AdguardHome</li>
+          </ul>
+          <h5>PERSONAL WEBSITE</h5>
+          <p>
+            Design, implement, and deploy a personal website with a clean UI/UX
+            and mobile-first responsiveness. Currently re- designing the
+            back-end with Django to support a blog.
+          </p>
+          <ul class="two-column">
+            <li>Vue3 and Bootstrap 5</li>
+            <li>Hosting provided by Hostinger.com</li>
+            <li>CI/CD with GitHub Actions</li>
+            <li>Version control with Git</li>
+          </ul>
         </section>
         <section id="list-item-5" class="mb-4">
-          <h4>Skills</h4>
+          <h3>SKILLS</h3>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat,
-            cumque voluptate vel sequi dolores eius dolore perferendis
-            accusantium. Harum atque iusto, alias id reiciendis tempore,
-            reprehenderit asperiores tempora in voluptas ea ducimus porro libero
-            deleniti esse quaerat exercitationem provident aliquam! Adipisci
-            nemo, praesentium nisi harum cupiditate facere corrupti nostrum
-            excepturi quas veniam ullam sit molestiae beatae aspernatur
-            provident, a voluptatum doloribus debitis iusto nesciunt vitae illum
-            quo cum. Reprehenderit dolorum quasi architecto aspernatur illum
-            sunt eveniet adipisci suscipit sit velit, consequuntur vel nostrum
-            nesciunt, culpa doloribus praesentium magnam porro harum ad
-            temporibus minus vero vitae voluptates cumque! Doloribus, dolorem
-            minus?
+            Foundational knowledge in core computing concepts, networking,
+            virtualization, cloud computing, and cybersecurity.<br />
+            Proven ability in communication, leadership, collaboration,
+            professionalism, and personal development.
           </p>
+          <ul class="two-column">
+            <li>Windows Home/Pro/Server</li>
+            <li>Microsoft 365</li>
+            <li>Microsoft Teams</li>
+            <li>RDP/VNC Remote connections</li>
+            <li>VS Code</li>
+            <li>SQL</li>
+            <li>SQL Server, SSMS</li>
+            <li>Python</li>
+            <li>IoT</li>
+            <li>Linux (Fedora, Debian)</li>
+            <li>Proxmox PVE</li>
+            <li>Shell Scripting (Bash)</li>
+            <li>VI, VIM, Nano</li>
+            <li>Docker</li>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>Java</li>
+            <li>JavaScript/HTML/CSS</li>
+          </ul>
         </section>
         <section id="list-item-6" class="mb-4">
-          <h4>Volunteer</h4>
+          <h3>VOLUNTEER ACTIVITIES</h3>
+          <h5>Skills USA Competition</h5>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quos
-            aspernatur eum recusandae quia, quo, aliquid magni odit voluptatibus
-            repellat tenetur obcaecati! Perferendis, possimus repudiandae?
-            Corrupti deleniti, ex rerum nesciunt ea, perspiciatis repellendus
-            necessitatibus unde eum repellat illo! Similique velit est fugit
-            esse? Autem nostrum laborum repudiandae consequatur cumque, nesciunt
-            tempora laboriosam explicabo enim voluptas expedita fuga vitae
-            beatae? Saepe, perspiciatis, quam dignissimos adipisci optio
-            excepturi ipsum eos harum voluptatem quaerat ex eligendi illum,
-            nobis soluta. Ipsa vel soluta, vitae iure vero, reiciendis laborum
-            inventore maiores omnis, distinctio totam explicabo. Quis quidem,
-            impedit quo suscipit nobis expedita soluta facilis velit....
+            Winston-Salem, NC
+            <br />
+            Aprtil 2024
+            <br />
+            Programming and Software Development Competitor
           </p>
+          <ul>
+            <li>
+              Competed in a state-level competition for programming and software
+              development.
+            </li>
+            <li>
+              Demonstrated ability to work in a high pressure environment with
+              little time to complete tasks.
+            </li>
+            <li>
+              Demonstrated problem-solving skills, and technical knowledge
+              through a 50 question quiz.
+            </li>
+          </ul>
+          <h5>ITP (Information Technology Professionals) Club</h5>
+          <p>
+            Winston-Salem, NC
+            <br />
+            August 2023 - Aprtil 2024
+            <br />
+            Club Member
+          </p>
+          <ul>
+            <li>
+              Engage in discussions about IT topics like AI, cybersecurity, and
+              programming during monthly meetings.
+            </li>
+            <li>
+              Participate in cybersecurity challenges and competitions,
+              developing teamwork and problem-solving skills.
+            </li>
+          </ul>
         </section>
       </div>
     </div>
@@ -202,10 +324,17 @@ export default {
 
 #resume-content {
   height: 100%;
+  margin-bottom: 70px;
 }
 
 section {
-  scroll-margin-bottom: 70px;
+  scroll-margin-bottom: 72px;
+}
+
+.two-column {
+  column-count: 2;
+  column-gap: 1rem;
+  list-style-position: inside;
 }
 
 @media (max-width: 768px) {
@@ -216,12 +345,16 @@ section {
     padding: 1rem;
     margin: 0 !important;
   }
-  p {
+  p,
+  li {
     font-size: 0.8rem;
   }
   .col-4,
   .col-8 {
     padding: 0.25rem !important;
+  }
+  #resume-content {
+    margin-bottom: 60px;
   }
 }
 </style>
